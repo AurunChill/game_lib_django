b2aba4f7e84373c4fe0cd421b40ebfed7fa29637
PoTtaTto <MrPotatsus@yandex.ru>
2024-05-04 00:31:26 +0300

    registration is added

--- app_users/forms.py
+++ app_users/forms.py
@@ -1,15 +1,15 @@
+from typing import Any
 from django import forms
 from django.contrib.auth import get_user_model, authenticate
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 UserModel = get_user_model()
 
 class UserLoginForm(AuthenticationForm):
-    def clean(self):
+    def clean(self) -> dict[str, Any]:
+        super().clean()
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
-
         if username and password:
             user = UserModel.objects.filter(email=username).first() or UserModel.objects.filter(username=username).first()
             if user:
@@ -18,5 +18,27 @@ class UserLoginForm(AuthenticationForm):
                     raise forms.ValidationError('Неверный пароль.', code='invalid_login')
             else:
                 raise forms.ValidationError('Пользователь не найден.', code='invalid_login')
+
+        return self.cleaned_data
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean(self) -> dict[str, Any]:
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password1')
+        password_repeated = self.cleaned_data.get('password2')
+        if username and email and password and password and password_repeated:
+            user_by_username = UserModel.objects.filter(username=username).first()
+            user_by_email = UserModel.objects.filter(email=email).first()
+            if not user_by_username and not user_by_email:
+                if password != password_repeated:
+                    raise forms.ValidationError('Пароли не совпадают', code='invalid_registration')
+            else:
+                raise forms.ValidationError('Аккаунт с таким именем/почтой уже существует', code='invalid_registration')
 
         return self.cleaned_data
--- app_users/views.py
+++ app_users/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 
 # Project
-from app_users.forms import UserLoginForm
+from app_users.forms import UserLoginForm, UserRegistrationForm
+
 
 def login(request: HttpRequest) -> HttpResponse:
     data = request.POST
@@ -12,7 +13,7 @@ def login(request: HttpRequest) -> HttpResponse:
         form = UserLoginForm(data=data)
         if form.is_valid():
             user = form.get_user()
-            auth_login(request, user)  # Log the user in
+            auth_login(request, user)
             return redirect(reverse('games:catalog'))  # Redirect user to home page or another appropriate page
     else:
         form = UserLoginForm()
@@ -30,8 +31,18 @@ def login(request: HttpRequest) -> HttpResponse:
 
 
 def registration(request: HttpRequest) -> HttpResponse:
+    data = request.POST
+    if data:
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('games:catalog'))  # Redirect to a different page after successful registration
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Регистрация'
+        'title': 'Регистрация',
+        'form': form
     }
 
     return render(
