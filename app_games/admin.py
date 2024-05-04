945146446e915161bdd027c7a127d563b57d37c9
PoTtaTto <MrPotatsus@yandex.ru>
2024-05-04 14:07:05 +0300

    adding image display in admin

--- app_games/admin.py
+++ app_games/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Project
 from app_games import models
@@ -7,8 +8,22 @@ from app_games import models
 @admin.register(models.GameModel)
 class GameModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'author', 'release_date', 'price', 'discount',)
+    list_display = ('show_cover', 'title', 'author', 'release_date', 'price', 'discount',)
     list_editable = ['discount',]
     search_fields = ['title', 'author', 'price', 'discount']
     list_filter = ['discount']
-    fields = ('author', 'image', 'title', 'slug', 'description', 'release_date', 'price', 'discount',)
+    readonly_fields=['show_cover_big']
+    fields = ('author', 'image', 'show_cover_big', 'title', 'slug', 'description', 'release_date', 'price', 'discount',)
+    save_on_top = True
+
+    @admin.display(description='Обложка')
+    def show_cover(self, game: models.GameModel):
+        if game.image:
+            return mark_safe(f'<img src="{game.image.url}" style="border-radius: 10px" width=50>')
+        return 'Нет обложки'
+
+    @admin.display(description='Обложка (Фото)')
+    def show_cover_big(self, game: models.GameModel):
+        if game.image:
+            return mark_safe(f'<img src="{game.image.url}" style="border-radius: 10px" width=250>')
+        return 'Нет обложки'
--- app_users/admin.py
+++ app_users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Project
 from app_users import models
@@ -6,7 +7,20 @@ from app_users import models
 
 @admin.register(models.UserModel)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["username", "first_name", "last_name", "email",]
+    list_display = ["username", 'show_avatar', "first_name", "last_name", "email",]
     search_fields = ["username", "first_name", "last_name", "email",]
+    readonly_fields=['show_avatar_big']
+    fields = ('username', 'email', 'password', 'image', 'show_avatar_big')
+    save_on_top = True
 
+    @admin.display(description='Аватар')
+    def show_avatar(self, user: models.UserModel):
+        if user.image:
+            return mark_safe(f'<img src="{user.image.url}" style="border-radius: 10px" width=35>')
+        return 'Нет аватарки'
     
+    @admin.display(description='Аватар (Фото)')
+    def show_avatar_big(self, user: models.UserModel):
+        if user.image:
+            return mark_safe(f'<img src="{user.image.url}" style="border-radius: 10px" width=250>')
+        return 'Нет аватарки'
