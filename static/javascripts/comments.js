a977e9b98907df1a5aba16d0a593ec5077b60441
PoTtaTto <MrPotatsus@yandex.ru>
2024-06-06 15:05:30 +0300

    road to comment reply

--- static/javascripts/comments.js
+++ static/javascripts/comments.js
@@ -1,5 +1,7 @@
 const api_url = 'http://127.0.0.1:8000/api/v1/comments/';
 
+let page_comments = null
+
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -48,10 +50,11 @@ $(document).ready(function() {
         method: 'GET',
         success: function(response) {
             console.log(response)
-            response.results.forEach((message) => {
+            page_comments = response.results
+            response.results.forEach((comment) => {
                 generate_comment(
-                    message.user.image, message.user.username,
-                    message.text, formatDateString(message.date)
+                    comment.user.image, comment.user.username,
+                    comment.text, formatDateString(comment.date)
                 );
             })
         },
@@ -73,8 +76,6 @@ $('#send_comment_form').on('submit', function(e) {
     const user_name = $(this).data('user_name');
     const text = $(this).find('textarea').val();
     const csrftoken = getCookie('csrftoken');
-
-    console.log(game_id, user_id, user_image_url, user_name, text, csrftoken);
 
     $(this).find('textarea').val('');
 
@@ -129,8 +130,28 @@ function generate_comment(image_url, user_name, text, date) {
     </div>
     <p class="text-gray-700 text-sm mb-2">${formatted_date}</p>
     <p class="text-gray-700">${text}</p>
+    <button class='flex items-center mt-4 cursor-pointer' onclick="${'aboba'}">
+            <p class="text-gray-600 text-sm">Ответить</p>
+            <div class='h-4 w-4 mt-1 ms-1 cursor-pointer'>
+                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M19.5 6.25C19.9142 6.25 20.25 6.58579 20.25 7C20.25 9.24444 19.298 10.7196 18.0632 11.6087C16.8667 12.4702 15.4534 12.75 14.5 12.75L6.31066 12.75L10.0303 16.4697C10.3232 16.7626 10.3232 17.2374 10.0303 17.5303C9.73744 17.8232 9.26256 17.8232 8.96967 17.5303L3.96967 12.5303C3.67678 12.2374 3.67678 11.7626 3.96967 11.4697L8.96967 6.46967C9.26256 6.17678 9.73744 6.17678 10.0303 6.46967C10.3232 6.76256 10.3232 7.23744 10.0303 7.53033L6.31066 11.25L14.5 11.25C15.2133 11.25 16.3 11.0298 17.1868 10.3913C18.0353 9.7804 18.75 8.75556 18.75 7C18.75 6.58579 19.0858 6.25 19.5 6.25Z"
+                            fill="#4b5563">
+                        </path>
+                    </g>
+                </svg>
+            </div>
+    </button>
 </div>
     `
 
     $('#comments_container').prepend(template);
+}
+
+
+function reply_to_comment() {
+    // generate_comment(user_image_url, user_name, text, date);
 }
