254b7b447f2899c481742a252edb4f1a4c84c634
PoTtaTto <MrPotatsus@yandex.ru>
2024-06-17 00:09:25 +0300

    fixing burger menu problem

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY=None
 SESSION_COOKIE_SECURE=False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -170,7 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
--- static/javascripts/base_page.js
+++ static/javascripts/base_page.js
@@ -1,43 +1,45 @@
-let $burger_menu = null
-let $dropdown_menu = null
+let burger_menu = null
+let dropdown_menu = null
 
 $(document).ready(function () {
-    $burger_menu = $('#burger_menu');
-    $dropdown_menu = $('#dropdown_menu');
+    burger_menu = $('#burgerMenu');
+    dropdown_menu = $('#dropdownMenu');
 
-    $dropdown_menu.addClass('hidden');
+    dropdown_menu.addClass('hidden');
 
-    $burger_menu.on('click', function () {
-        $dropdown_menu.toggleClass('hidden');
+    burger_menu.on('click', function () {
+        dropdown_menu.toggleClass('hidden');
     });
 });
 
 $(document).on('click', function (event) {
-    const $target = $(event.target);
-    const is_outside_dropdown = !$dropdown_menu.has($target).length && $target[0] !== $dropdown_menu[0];
-    const is_not_burger_menu = $target[0] !== $burger_menu[0];
-    const does_not_have_ripple_class = !$target.hasClass('ripple');
-    const is_not_svg = $target.prop('tagName').toLowerCase() !== 'svg';
+    console.log('clicking')
+    const target = $(event.target);
+    const is_outside_dropdown = !dropdown_menu.has(target).length && target[0] !== dropdown_menu[0];
+    const is_not_burger_menu = target[0] !== burger_menu[0];
+    const does_not_have_ripple_class = !target.hasClass('ripple');
+    const is_not_svg = target.prop('tagName').toLowerCase() !== 'svg';
 
+    console.log(is_outside_dropdown, is_not_burger_menu, does_not_have_ripple_class, is_not_svg)
     if (is_outside_dropdown && is_not_burger_menu && does_not_have_ripple_class && is_not_svg) {
-        $dropdown_menu.addClass('hidden');
+        dropdown_menu.addClass('hidden');
     }
 });
 
 function createRipple(event) {
-    const $button = $(event.currentTarget);
-    const $circle = $('');
-    const diameter = Math.max($button.outerWidth(), $button.outerHeight());
+    const button = $(event.currentTarget);
+    const circle = $('');
+    const diameter = Math.max(button.outerWidth(), button.outerHeight());
     const radius = diameter / 2;
 
-    $circle.css({
+    circle.css({
         width: diameter,
         height: diameter,
-        left: event.clientX - $button.offset().left - radius,
-        top: event.clientY - $button.offset().top - radius
+        left: event.clientX - button.offset().left - radius,
+        top: event.clientY - button.offset().top - radius
     });
 
-    $button.append($circle);
+    button.append(circle);
 
-    setTimeout(() => $circle.remove(), 600); // Adjust to match the animation duration!
+    setTimeout(() => circle.remove(), 600); // Adjust to match the animation duration!
 }
